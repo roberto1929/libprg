89ee9d460daec33cabb58e7504accd0a799d0bac
Roberto da Silva Espindola <roberto.se@aluno.ifsc.edu.br>
2023-09-05 09:20:10 -0300

    Começando exercicios

--- libprg/src/include/libprg/libprg.h
+++ libprg/src/include/libprg/libprg.h
@@ -1,6 +1,8 @@
 #ifndef LIBPRG_LIBPRG_H
 #define LIBPRG_LIBPRG_H
 
+#include <string.h>
+
 typedef struct {
     double value;
     int error;
@@ -11,5 +13,18 @@ typedef enum { SUM, SUB } operation_t;
 result_t sub(double a, double b);
 result_t sum(double a, double b);
 result_t compute(double a, double b, operation_t op);
+
+/**
+ * g  jgjh gjh gjh gj
+ * @param elemento
+ * @param vetor
+ * @param tam
+ * @param total
+ * @return
+ */
+int inserir(int elemento, int *vetor, int tam, int *total);
+// função de remover
+int remover(int elemento);
+int busca_linear();
 
 #endif
---
+++ libprg/src/libprg/lista_linear.c
@@ -0,0 +1,2 @@
+#include <libprg/libprg.h>
+
