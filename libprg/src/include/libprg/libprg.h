a7080ae1a12c0f45072a2ef126dbf921b04ded7a
Roberto da Silva Espindola <roberto.se@aluno.ifsc.edu.br>
2023-09-11 11:24:17 -0300

    Atualizado - Aula 11/09

--- libprg/src/include/libprg/libprg.h
+++ libprg/src/include/libprg/libprg.h
@@ -1,30 +1,58 @@
 #ifndef LIBPRG_LIBPRG_H
 #define LIBPRG_LIBPRG_H
 
-#include <string.h>
+#include <stdio.h>
+#include <limits.h>
+#include <stdlib.h>
+#include <time.h>
 
-typedef struct {
-    double value;
-    int error;
-} result_t;
-
-typedef enum { SUM, SUB } operation_t;
-
-result_t sub(double a, double b);
-result_t sum(double a, double b);
-result_t compute(double a, double b, operation_t op);
 
 /**
- * g  jgjh gjh gjh gj
- * @param elemento
- * @param vetor
- * @param tam
- * @param total
+ * para criar uma lista linear de alocação sequencial ordenada
+ * @param vetor lista a ser criada
+ * @param tam tamamho da lista
+ * @return 0 se criou e -1 se não conseguiu
+ */
+
+int criar_lista(int *vetor, int tam);
+
+/**
+ * para inserir um elemento na lista não ordenada e não ordenada
+ * @param elemento elemento a ser inserido
+ * @param vetor lista
+ * @param tam tamanho da lista
+ * @param total total de elementos da lista
  * @return
  */
 int inserir(int elemento, int *vetor, int tam, int *total);
-// função de remover
-int remover(int elemento);
-int busca_linear();
+int inserir_ordenada(int elemento, int *vetor, int tam, int *total);
+
+/**
+ * para remover um elemento na lista ordenada e não ordenada
+ * @param elemento elemento inserido pelo user
+ * @param vetor lista
+ * @param total total de elementos
+ * @return
+ */
+int remover(int elemento, int *vetor, int *total);
+int remover_ordenada(int elemento, int *vetor, int *total);
+
+/**
+ * para buscar o elemento na lista
+ * @param elemento elemento inserido
+ * @param vetor lista criada
+ * @return
+ */
+int busca_linear(int elemento, int *vetor);
+int busca_binaria_ite(int elemento, int *vetor);
+int busca_binaria_rec(int elemento, int *vetor);
+
+/**
+ *
+ * @param vetor lista criada
+ * @param tam tamanho da lista
+ * @return
+ */
+int destruir(int *vetor, int tam);
 
 #endif
--- libprg/src/libprg/lista_linear.c
+++ libprg/src/libprg/lista_linear.c
@@ -1,2 +1,38 @@
 #include <libprg/libprg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
+int criar_lista(int *vetor, int tam){
+    srand(time(NULL));
+
+
+}
+
+
+int remover(int elemento, int *vetor, int *total){
+
+}
+
+int remover_ordenada(int elemento, int *vetor, int *total){
+
+}
+
+
+int inserir(int elemento, int *vetor, int tam, int *total){
+
+}
+
+
+int busca_linear(int elemento, int *vetor){
+
+}
+
+int busca_binaria_ite(int elemento, int *vetor){
+
+}
+
+
+int busca_binaria_rec(int elemento, int *vetor){
+
+}
