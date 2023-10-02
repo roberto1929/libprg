dd7b9fd4d26785cbd5f930adc6daf8d12815af36
Roberto da Silva Espindola <127215401+roberto1929@users.noreply.github.com>
2023-10-02 20:42:20 -0300

    Update lista_encadeada.c

--- libprg/src/libprg/lista_encadeada.c
+++ libprg/src/libprg/lista_encadeada.c
@@ -3,6 +3,45 @@
 //
 #include <libprg/libprg.h>
 
-no_t* criar_e(int dado){
-    no_t n = {.elemento = dado, .proximo = NULL};
+no_t criar_lista_e(int dado){
+    
 }
+
+no_t* inserir_e(no_t* inicio, int dado){
+  no_t∗ novo = (no_t∗) malloc(sizeof (no_t));
+novo−>dado = dado;
+novo−>proximo = ∗inicio;
+∗inicio = novo;
+}
+
+no_t* excluir_e(no_t∗∗ inicio, int dado){
+    no_t∗ atual = ∗inicio;
+    no_t∗ prox;
+    while(atual != NULL){
+        prox = atual−>proximo;
+        free(atual);
+        atual = prox;
+    }
+    ∗inicio = NULL;
+}
+
+no_t* busca_e(no_t* inicio, int dado) {
+    if (inicio == NULL) {
+        return NULL; 
+    }
+    no_t* atual = inicio;
+    do {
+        if (atual->dados == dado) {
+            return atual;
+        }
+        atual = atual->proximo;
+    } while (atual != inicio);
+    return NULL;
+}
+]
+
+
+
+
+
+
