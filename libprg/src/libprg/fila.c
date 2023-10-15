408f28ca1a0a94174bbd3bc2354fdf4090491cad
Roberto da Silva Espindola <127215401+roberto1929@users.noreply.github.com>
2023-10-15 18:28:39 -0300

    Update fila.c

--- libprg/src/libprg/fila.c
+++ libprg/src/libprg/fila.c
@@ -1,65 +1,46 @@
 #include <libprg/libprg.h>
 
-int criar_fila(fila_t *vetor){
-    vetor->vetor = (int* ) calloc(vetor->tamamho, sizeof(int));
-    vetor->inicio = -1;
-    vetor->fim = -1;
+int enqueue(int elemento, fila_t *fila){
+    if(fila->tamanho == fila->total){
+        return 1;
+    }
+    fila->vetor[fila->fim] = elemento;
+    fila->total++;
+    fila->fim = (fila->fim + 1)% fila ->tamanho;
+    return 0;
 }
 
-int enqueue(int elemento, fila_t *vetor){
-    if(full(vetor)){
-        return 0;
-    }
-    if(empty(vetor)){
-        vetor->inicio = 0;
-    }
-    vetor->fim = (vetor->fim+1)%100;
-    vetor->vetor[vetor->fim] = elemento;
-    return 1;
+int dequeue( fila_t *fila){
+    if(empty(fila)){
+        return 1;
+    };
+    fila->inicio = (fila->inicio + 1) % fila->tamanho;
+    fila->total--;
+    return fila->vetor[fila->fim];
 }
 
-int dequeue( fila_t *vetor){
-    if(empty(vetor)){
-        return -1;
-    }
-    int elemento_removido = vetor->vetor[vetor->inicio];
-    if(vetor->inicio == vetor->fim){
-        criar_fila(vetor);
-    } else {
-        vetor->inicio = (vetor->inicio +1)%100;
-    }
-    return  elemento_removido;
-}
-
-int head(fila_t *vetor){
-    if(empty(vetor)){
-        return -1;
-    }
+int head(fila_t *fila){
     return vetor->vetor[vetor->inicio];
 }
 
-int tail(fila_t *vetor){
-    if(empty(vetor)){
-        return -1;
-    }
-    return vetor->vetor[vetor->fim];
+int tail(fila_t *fila){
+    return fila->vetor[vetor->fim];
 }
 
 int size(fila_t *vetor){
-    if(empty(vetor)){
+    return fila->total;
+}
+
+int empty(fila_t *fila){
+    if(fila->total == 0){
         return 0;
     }
-    if(vetor->inicio <= vetor->fim){
-        return vetor->fim - vetor->inicio +1;
-    } else{
-        return 100 - vetor->inicio + vetor->fim +1;
+    return 1;
+}
+
+int full(fila_t *fila){
+    if(fila->total == fila->tamanho){
+        return 0;
     }
-}
-
-int empty(fila_t *vetor){
-    return (vetor->inicio == -1 && vetor->fim == -1);
-}
-
-int full(fila_t *vetor){
-    return ((vetor->fim +1) % 100 == vetor->inicio);
+    return 1;
 }
