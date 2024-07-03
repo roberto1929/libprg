80a968c24115f6140b8e4eb3e49d44a85454d553
Roberto da Silva Espindola <roberto.se@aluno.ifsc.edu.br>
2024-07-03 11:12:57 -0300

    temp

--- libprg/src/libprg/lista_contato.c
+++ libprg/src/libprg/lista_contato.c
@@ -1,104 +1,104 @@
-#include <libprg/libprg.h>
-#include <string.h>
-
-#define TAMANHO_P 10
-
-contato_t* criar_contato(int tamanho){
-
-    contato_t* contato = (contato_t*) malloc(sizeof (&contato));
-    if (contato == NULL) {
-        printf("Erro ao alocar memória para o contato.\n");
-        return NULL;
-    }
-    contato->vetor = (pessoa_t *) malloc(sizeof (pessoa_t) * tamanho);
-    if (contato->vetor == NULL) {
-        printf("Erro ao alocar memória para o vetor de pessoas.\n");
-        free(contato);
-        return NULL;
-    }
-    contato->tamanho = tamanho;
-    contato->total = 0;
-    return contato;
-}
-
-bool adicionar_pessoa(contato_t* contato, char nome[100], char email[50], char telefone[15]){
-    if (contato->total < contato->tamanho) {
-        pessoa_t nova_pessoa;
-        strcpy(nova_pessoa.nome, nome);
-        strcpy(nova_pessoa.email, email);
-        strcpy(nova_pessoa.telefone, telefone);
-        contato->vetor[contato->total++] = nova_pessoa;
-        return true;
-    } else {
-        return false;
-    }
-}
-
-
-void imprimir_contatos(contato_t *contato){
-    printf("Contatos:\n");
-        for (int i = 0; i < contato->total; ++i) {
-            printf("[%d] Nome: %s\n", i, contato->vetor[i].nome);
-            printf("    Telefone: %s\n", contato->vetor[i].telefone);
-            printf("    Email: %s\n\n", contato->vetor[i].email);
-    }
-}
-
-
-int buscar_contato(contato_t* lista_contatos, char nome[100], int* resultados) {
-    int contagem = 0;
-    for (int i = 0; i < lista_contatos->total; ++i) {
-        if (strcasestr(lista_contatos->vetor[i].nome, nome) != NULL) {
-            resultados[contagem] = i;
-            contagem++;
-        }
-    }
-    return contagem;
-}
-int editar_contato(contato_t *contato, int pos_lista, char *nome, char *email, char *telefone) {
-    if (pos_lista >= 0 && pos_lista < contato->total) {
-        strcpy(contato->vetor[pos_lista].nome, nome);
-        strcpy(contato->vetor[pos_lista].telefone, telefone);
-        strcpy(contato->vetor[pos_lista].email, email);
-        return 1;
-    } else {
-        return 0;
-    }
-}
-
-
-int excluir_contato(contato_t *contato, int indice) {
-    if (indice < 0 || indice >= contato->total) {
-        return 0;
-    }
-
-    for (int i = indice; i < contato->total - 1; i++) {
-        contato->vetor[i] = contato->vetor[i+1];
-    }
-    contato->total --;
-    return 1;
-}
-void salvar_contatos(contato_t* lista_contatos){
-    FILE* arquivo = fopen("contatos.dat", "wb");
-    if (arquivo == NULL) {
-        printf("Erro ao abrir o arquivo %s.\n", "contatos.dat");
-        return;
-    }
-
-    fwrite(&lista_contatos->total, sizeof(int), 1, arquivo);
-    fwrite(lista_contatos->vetor, sizeof(pessoa_t), lista_contatos->total, arquivo);
-
-    fclose(arquivo);
-}
-void carregar_contatos(contato_t* lista_contatos){
-    FILE* arquivo = fopen("contatos.dat", "rb");
-    if (arquivo == NULL) {
-        printf("Arquivo %s nao encontrado ou vazio. Criando novo arquivo...\n", "contatos.dat");
-        return;
-    }
-
-    fread(&lista_contatos->total, sizeof(int), 1, arquivo);
-    fread(lista_contatos->vetor, sizeof(pessoa_t), lista_contatos->total, arquivo);
-
-    fclose(arquivo);
-}
+//#include <libprg/libprg.h>
+//#include <string.h>
+//
+//#define TAMANHO_P 10
+//
+//contato_t* criar_contato(int tamanho){
+//
+//    contato_t* contato = (contato_t*) malloc(sizeof (&contato));
+//    if (contato == NULL) {
+//        printf("Erro ao alocar memória para o contato.\n");
+//        return NULL;
+//    }
+//    contato->vetor = (pessoa_t *) malloc(sizeof (pessoa_t) * tamanho);
+//    if (contato->vetor == NULL) {
+//        printf("Erro ao alocar memória para o vetor de pessoas.\n");
+//        free(contato);
+//        return NULL;
+//    }
+//    contato->tamanho = tamanho;
+//    contato->total = 0;
+//    return contato;
+//}
+//
+//bool adicionar_pessoa(contato_t* contato, char nome[100], char email[50], char telefone[15]){
+//    if (contato->total < contato->tamanho) {
+//        pessoa_t nova_pessoa;
+//        strcpy(nova_pessoa.nome, nome);
+//        strcpy(nova_pessoa.email, email);
+//        strcpy(nova_pessoa.telefone, telefone);
+//        contato->vetor[contato->total++] = nova_pessoa;
+//        return true;
+//    } else {
+//        return false;
+//    }
+//}
+//
+//
+//void imprimir_contatos(contato_t *contato){
+//    printf("Contatos:\n");
+//        for (int i = 0; i < contato->total; ++i) {
+//            printf("[%d] Nome: %s\n", i, contato->vetor[i].nome);
+//            printf("    Telefone: %s\n", contato->vetor[i].telefone);
+//            printf("    Email: %s\n\n", contato->vetor[i].email);
+//    }
+//}
+//
+//
+//int buscar_contato(contato_t* lista_contatos, char nome[100], int* resultados) {
+//    int contagem = 0;
+//    for (int i = 0; i < lista_contatos->total; ++i) {
+//        if (strcasestr(lista_contatos->vetor[i].nome, nome) != NULL) {
+//            resultados[contagem] = i;
+//            contagem++;
+//        }
+//    }
+//    return contagem;
+//}
+//int editar_contato(contato_t *contato, int pos_lista, char *nome, char *email, char *telefone) {
+//    if (pos_lista >= 0 && pos_lista < contato->total) {
+//        strcpy(contato->vetor[pos_lista].nome, nome);
+//        strcpy(contato->vetor[pos_lista].telefone, telefone);
+//        strcpy(contato->vetor[pos_lista].email, email);
+//        return 1;
+//    } else {
+//        return 0;
+//    }
+//}
+//
+//
+//int excluir_contato(contato_t *contato, int indice) {
+//    if (indice < 0 || indice >= contato->total) {
+//        return 0;
+//    }
+//
+//    for (int i = indice; i < contato->total - 1; i++) {
+//        contato->vetor[i] = contato->vetor[i+1];
+//    }
+//    contato->total --;
+//    return 1;
+//}
+//void salvar_contatos(contato_t* lista_contatos){
+//    FILE* arquivo = fopen("contatos.dat", "wb");
+//    if (arquivo == NULL) {
+//        printf("Erro ao abrir o arquivo %s.\n", "contatos.dat");
+//        return;
+//    }
+//
+//    fwrite(&lista_contatos->total, sizeof(int), 1, arquivo);
+//    fwrite(lista_contatos->vetor, sizeof(pessoa_t), lista_contatos->total, arquivo);
+//
+//    fclose(arquivo);
+//}
+//void carregar_contatos(contato_t* lista_contatos){
+//    FILE* arquivo = fopen("contatos.dat", "rb");
+//    if (arquivo == NULL) {
+//        printf("Arquivo %s nao encontrado ou vazio. Criando novo arquivo...\n", "contatos.dat");
+//        return;
+//    }
+//
+//    fread(&lista_contatos->total, sizeof(int), 1, arquivo);
+//    fread(lista_contatos->vetor, sizeof(pessoa_t), lista_contatos->total, arquivo);
+//
+//    fclose(arquivo);
+//}
--- libprg/src/libprg/lista_encadeada.c
+++ libprg/src/libprg/lista_encadeada.c
@@ -1,188 +1,188 @@
-#include <libprg/libprg.h>
-
-
-void adiciona_lista(listaEncadeada_t *queue, no_t** no, int valor){
-    no_t *novo = (no_t*) malloc(sizeof(no_t));
-    novo->dado = valor;
-    novo->proximo = NULL;
-
-    if(*no == NULL){
-        *no = novo;
-        queue->inicio = *no;
-        queue->size++;
-    } else {
-        no_t * atual = *no;
-        while (atual->proximo != NULL){
-            atual = atual->proximo;
-        }
-        atual->proximo = novo;
-        queue->size++;
-    }
-
-}
-
-bool remove_lista(no_t** inicio, int dado, listaEncadeada_t * queue){
-    no_t* atual = *inicio;
-    no_t* anterior = NULL;
-    while (atual != NULL){
-        if(atual->dado == dado){
-            if(anterior == NULL){
-                *inicio = atual->proximo;
-            } else {
-                anterior->proximo = atual->proximo;
-            }
-            free(atual);
-            queue->size--;
-            return true;
-        }
-        anterior = atual;
-        atual = atual->proximo;
-    }
-    return false;
-}
-
-no_t* busca_lista(no_t* inicio, int dado){
-    while (inicio != NULL){
-        if(inicio->dado == dado){
-            return inicio;
-        }
-        inicio = inicio->proximo;
-    }
-    return NULL;
-}
-
-void adiciona_pilha(stacklist_t *stack, no_t **no, int elemento){
-    no_t *novo = (no_t*) malloc(sizeof(no_t));
-    if(novo == NULL){
-        return;
-    }
-
-    novo->dado = elemento;
-    novo->proximo = *no;
-    *no = novo;
-    stack->topo = novo;
-    stack->size++;
-}
-int remove_pilha(stacklist_t *stack, no_t **no){
-    if(*no == NULL){
-        return 0;
-    }
-    no_t  *atual = stack->topo;
-    *no = stack->topo->proximo;
-    free(atual);
-    stack->topo = *no;
-    stack->size--;
-    return 1;
-
-}
-
-no_t *busca_pilha(no_t* inicio, int dado){
-    while (inicio != NULL){
-        if(inicio->dado == dado){
-            return inicio;
-        }
-        inicio = inicio->proximo;
-    }
-    return NULL;
-}
-
-
-
-
-void addCircle(no_t **node, bool op, int value) {
-    no_t *new = (no_t *) malloc(sizeof(no_t));
-    new->dado = value;
-    new->proximo = *node;
-
-    if (*node == NULL) {
-        *node = new;
-        return;
-    }
-    if(!op){
-        no_t *atual = *node;
-        while (atual->proximo != *node){
-            atual = atual->proximo;
-        }
-        atual->proximo = new;
-    } else {
-        no_t* atual = *node;
-        while (atual->proximo != *node && atual->dado > value){
-            atual = atual->proximo;
-        }
-        new->proximo = atual->proximo;
-        atual->proximo = new;
-    }
-}
-
-int remove_circular(no_t **node, int value){
-
-    if(*node == NULL){
-        return -1;
-    }
-    no_t *current = *node;
-    while(current != NULL){
-        if(current->proximo->dado == value){
-            current->proximo = current->proximo->proximo;
-            return 1;
-        }
-        current = current->proximo;
-    }
-    return 0;
-
-}
-
-void destruir(no_t** no){
-    no_t* atual = *no;
-    no_t* prox;
-    while (atual != NULL){
-        prox = atual->proximo;
-        free(atual);
-        atual = prox;
-    }
-    *no = NULL;
-}
-
-
-void imprime_no(no_t *no){
-    while (no != NULL){
-        printf("%d ", no->dado);
-        no = no->proximo;
-    }
-}
-
-// Duplamente encadeada
-
-bool criar_d(int dado){
-    nod_t *inicio = malloc(sizeof (nod_t));
-    if(inicio == NULL){
-        return false;
-    }
-    inicio->elemento = dado;
-    inicio->proximo = NULL;
-    inicio->anterior = NULL;
-    return true;
-}
-
-nod_t add_d(nod_t *nod, int dado){
-    nod_t *novo = malloc(sizeof (novo));
-    novo->elemento = dado;
-    nod->anterior = novo->proximo;
-    novo->proximo = nod->proximo;
-    nod->proximo = (struct nod_t *) novo;
-    return *novo;
-}
-
-nod_t * add_do(nod_t *nod, int dado){
-    nod_t *novo = malloc(sizeof (nod_t));
-    novo->elemento = dado;
-    while (nod->proximo != NULL){
-        if(dado < nod->elemento){
-            novo->proximo = nod;
-            novo->anterior = nod->anterior;
-            nod->anterior = novo;
-        }else{
-            novo->proximo = nod;
-        }
-    }
-    return nod;
-}
+//#include <libprg/libprg.h>
+//
+//
+//void adiciona_lista(listaEncadeada_t *queue, no_t** no, int valor){
+//    no_t *novo = (no_t*) malloc(sizeof(no_t));
+//    novo->dado = valor;
+//    novo->proximo = NULL;
+//
+//    if(*no == NULL){
+//        *no = novo;
+//        queue->inicio = *no;
+//        queue->size++;
+//    } else {
+//        no_t * atual = *no;
+//        while (atual->proximo != NULL){
+//            atual = atual->proximo;
+//        }
+//        atual->proximo = novo;
+//        queue->size++;
+//    }
+//
+//}
+//
+//bool remove_lista(no_t** inicio, int dado, listaEncadeada_t * queue){
+//    no_t* atual = *inicio;
+//    no_t* anterior = NULL;
+//    while (atual != NULL){
+//        if(atual->dado == dado){
+//            if(anterior == NULL){
+//                *inicio = atual->proximo;
+//            } else {
+//                anterior->proximo = atual->proximo;
+//            }
+//            free(atual);
+//            queue->size--;
+//            return true;
+//        }
+//        anterior = atual;
+//        atual = atual->proximo;
+//    }
+//    return false;
+//}
+//
+//no_t* busca_lista(no_t* inicio, int dado){
+//    while (inicio != NULL){
+//        if(inicio->dado == dado){
+//            return inicio;
+//        }
+//        inicio = inicio->proximo;
+//    }
+//    return NULL;
+//}
+//
+//void adiciona_pilha(stacklist_t *stack, no_t **no, int elemento){
+//    no_t *novo = (no_t*) malloc(sizeof(no_t));
+//    if(novo == NULL){
+//        return;
+//    }
+//
+//    novo->dado = elemento;
+//    novo->proximo = *no;
+//    *no = novo;
+//    stack->topo = novo;
+//    stack->size++;
+//}
+//int remove_pilha(stacklist_t *stack, no_t **no){
+//    if(*no == NULL){
+//        return 0;
+//    }
+//    no_t  *atual = stack->topo;
+//    *no = stack->topo->proximo;
+//    free(atual);
+//    stack->topo = *no;
+//    stack->size--;
+//    return 1;
+//
+//}
+//
+//no_t *busca_pilha(no_t* inicio, int dado){
+//    while (inicio != NULL){
+//        if(inicio->dado == dado){
+//            return inicio;
+//        }
+//        inicio = inicio->proximo;
+//    }
+//    return NULL;
+//}
+//
+//
+//
+//
+//void addCircle(no_t **node, bool op, int value) {
+//    no_t *new = (no_t *) malloc(sizeof(no_t));
+//    new->dado = value;
+//    new->proximo = *node;
+//
+//    if (*node == NULL) {
+//        *node = new;
+//        return;
+//    }
+//    if(!op){
+//        no_t *atual = *node;
+//        while (atual->proximo != *node){
+//            atual = atual->proximo;
+//        }
+//        atual->proximo = new;
+//    } else {
+//        no_t* atual = *node;
+//        while (atual->proximo != *node && atual->dado > value){
+//            atual = atual->proximo;
+//        }
+//        new->proximo = atual->proximo;
+//        atual->proximo = new;
+//    }
+//}
+//
+//int remove_circular(no_t **node, int value){
+//
+//    if(*node == NULL){
+//        return -1;
+//    }
+//    no_t *current = *node;
+//    while(current != NULL){
+//        if(current->proximo->dado == value){
+//            current->proximo = current->proximo->proximo;
+//            return 1;
+//        }
+//        current = current->proximo;
+//    }
+//    return 0;
+//
+//}
+//
+//void destruir(no_t** no){
+//    no_t* atual = *no;
+//    no_t* prox;
+//    while (atual != NULL){
+//        prox = atual->proximo;
+//        free(atual);
+//        atual = prox;
+//    }
+//    *no = NULL;
+//}
+//
+//
+//void imprime_no(no_t *no){
+//    while (no != NULL){
+//        printf("%d ", no->dado);
+//        no = no->proximo;
+//    }
+//}
+//
+//// Duplamente encadeada
+//
+//bool criar_d(int dado){
+//    nod_t *inicio = malloc(sizeof (nod_t));
+//    if(inicio == NULL){
+//        return false;
+//    }
+//    inicio->elemento = dado;
+//    inicio->proximo = NULL;
+//    inicio->anterior = NULL;
+//    return true;
+//}
+//
+//nod_t add_d(nod_t *nod, int dado){
+//    nod_t *novo = malloc(sizeof (novo));
+//    novo->elemento = dado;
+//    nod->anterior = novo->proximo;
+//    novo->proximo = nod->proximo;
+//    nod->proximo = (struct nod_t *) novo;
+//    return *novo;
+//}
+//
+//nod_t * add_do(nod_t *nod, int dado){
+//    nod_t *novo = malloc(sizeof (nod_t));
+//    novo->elemento = dado;
+//    while (nod->proximo != NULL){
+//        if(dado < nod->elemento){
+//            novo->proximo = nod;
+//            novo->anterior = nod->anterior;
+//            nod->anterior = novo;
+//        }else{
+//            novo->proximo = nod;
+//        }
+//    }
+//    return nod;
+//}
