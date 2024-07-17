e8b327a85a66c6093631bc38e683811a302d072d
Roberto da Silva Espindola <roberto.se@aluno.ifsc.edu.br>
2024-07-17 10:17:23 -0300

    Atualizando `lista_ord.c`

--- libprg/src/libprg/lista_ord.c
+++ libprg/src/libprg/lista_ord.c
@@ -82,9 +82,9 @@ void merge_sort(sort_t* sort, int esquerda, int direita){
 }
 
 void merge(sort_t* sort, int meio, int esquerda, int direita){
-    printf("Aqui\n");
+    int temp = direita - esquerda + 1;
+    printf("%d \n", temp);
     int *aux = (int *) malloc((direita - esquerda + 1) * sizeof(int));
-    printf("Ali\n");
     int i = esquerda;
     int j = meio + 1;
     int k = 0;
